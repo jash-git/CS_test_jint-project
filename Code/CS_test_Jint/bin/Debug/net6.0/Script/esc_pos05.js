9054cf8d9f57f2a72456768e8471bfa5084824de
jash.liao <jash.liao@gmail.com>
2023-01-04 17:39:21 +0800

    modified 202301041738

--- Code/CS_test_Jint/bin/Debug/net6.0/Script/esc_pos05.js
+++ Code/CS_test_Jint/bin/Debug/net6.0/Script/esc_pos05.js
@@ -1,4 +1,4 @@
-//付款明細
+//Bill~80mm
 function Main() {
     //JSON資料顯示格式轉換: https://jsonformatter.org/
     /*
@@ -35,14 +35,22 @@ function Main() {
 
     //---
     //新增列印主體內容
-    ESC_Value.push(ecTEXT_ALIGN_CENTER + ecBOLD_ON + ecBIG_ON + json_obj.company_shortname + ecBIG_OFF + ecBOLD_OFF + ecFREE_LINE + ecFREE_LINE);//文字至中 + 粗體+放大 + 店名 + 換行
+	
+	//店名;文字至中 + 粗體+放大 + 店名 + 換行
+    ESC_Value.push(ecTEXT_ALIGN_CENTER + ecBOLD_ON + ecBIG_ON + json_obj.store_name + ecBIG_OFF + ecBOLD_OFF + ecFREE_LINE + ecFREE_LINE);
 
+	//單號;文字靠左 + 放大 + 單號 + 換行
     strbuf = '單號(' + json_obj.order_type_name + ') :' + json_obj.call_num;
-    ESC_Value.push(ecTEXT_ALIGN_LEFT + ecBIG_ON + strbuf + ecBIG_OFF + ecFREE_LINE);//文字靠左 + 放大 + 單號 + 換行
+    ESC_Value.push(ecTEXT_ALIGN_LEFT + ecBIG_ON + strbuf + ecBIG_OFF + ecFREE_LINE);
 
-    strbuf = '桌號: ' + json_obj.table_name;
-    ESC_Value.push(ecTEXT_ALIGN_LEFT + ecBIG_ON + strbuf + ecBIG_OFF + ecFREE_LINE);//文字靠左 + 放大 + 桌號 + 換行
+	//桌號;文字靠左 + 放大 + 桌號 + 換行
+	if(json_obj.table_name.length>0)
+	{
+		strbuf = '桌號: ' + json_obj.table_name;
+		ESC_Value.push(ecTEXT_ALIGN_LEFT + ecBIG_ON + strbuf + ecBIG_OFF + ecFREE_LINE);		
+	}
 
+	//日期&時間;文字靠左 + 日期(時間) + 換行
     var date = new Date(json_obj.order_time * 1000);//json_obj.order_time (sec) -> ms, https://www.fooish.com/javascript/date/
     var month = pad2(date.getMonth() + 1);//months (0-11)
     var day = pad2(date.getDate());//day (1-31)
@@ -50,57 +58,66 @@ function Main() {
     var hour = pad2(date.getHours());
     var minute = date.getMinutes();
     strbuf = '日期: ' + year + "-" + month + "-" + day + "  時間: " + hour + ':' + minute;
-    ESC_Value.push(ecTEXT_ALIGN_LEFT + strbuf + ecFREE_LINE);//文字靠左 + 日期(時間) + 換行
+    ESC_Value.push(ecTEXT_ALIGN_LEFT + strbuf + ecFREE_LINE);
 
+	//交易序號;文字靠左 + 交易序號 + 換行
     strbuf = '交易序號: ' + json_obj.order_no;
-    ESC_Value.push(ecTEXT_ALIGN_LEFT + strbuf + ecFREE_LINE);//文字靠左 + 交易序號 + 換行
+    ESC_Value.push(ecTEXT_ALIGN_LEFT + strbuf + ecFREE_LINE);
 
+	//分隔線;文字靠左 + 分隔線 + 換行(80mm分隔線48的符號)
     strbuf = '------------------------------------------------';
+    ESC_Value.push(ecTEXT_ALIGN_LEFT + strbuf + ecFREE_LINE);
+
+    strbuf = '商品名稱                            數量    價格';//48=8[中文4個字]+32+4[中文2個字]+4+4[中文2個字]
     ESC_Value.push(ecTEXT_ALIGN_LEFT + strbuf + ecFREE_LINE);//文字靠左 + 分隔線 + 換行
 
-    strbuf = '商品名稱                       數  量    價  格';//48=32+6+4+6
-    ESC_Value.push(ecTEXT_ALIGN_LEFT + strbuf + ecFREE_LINE);//文字靠左 + 分隔線 + 換行
-
+	//分隔線;文字靠左 + 分隔線 + 換行(80mm分隔線48的符號)
     strbuf = '------------------------------------------------';
-    ESC_Value.push(ecTEXT_ALIGN_LEFT + strbuf + ecFREE_LINE);//文字靠左 + 分隔線 + 換行
+    ESC_Value.push(ecTEXT_ALIGN_LEFT + strbuf + ecFREE_LINE);
+	
     var AllCount = 0;
-
+	var space = "";
+	var spaceCount = 0;
     //---
     //產品+配料
     if (json_obj.order_items != null) {
         for (var i = 0; i < json_obj.order_items.length; i++) {
-            //6字(X000)
-            var space = "";
-            var spaceCount = 0;
-
-            if ((json_obj.order_items[i].product_type == 'P') || (json_obj.order_items[i].product_type == 'K')) {//一般餐點和包材
-                AllCount += json_obj.order_items[i].count;
-                var count = "" + json_obj.order_items[i].count;
-                spaceCount = 6 - Wlen(count) - 2;
-                for (var j = 0; j < spaceCount; j++) {
-                    space += " ";
+			space = "";
+			spaceCount = 0;
+			
+            if ((json_obj.order_items[i].product_type == 'P') || (json_obj.order_items[i].product_type == 'K')) 
+			{//一般餐點和包材
+                AllCount += json_obj.order_items[i].count;//總數量統計
+				
+                var count = "" + json_obj.order_items[i].count;//單一產品數量值轉字串
+                spaceCount = 6 - Wlen(count) - 2;//計算數量欄位的空白數= 該欄位總長度6 - 數量字串長度 - X符號長度
+                for (var j = 0; j < spaceCount; j++){
+                    space += " ";//產生對應空白字串
                 }
                 count = "X" + space + json_obj.order_items[i].count;
 
                 space = "";
-                var amount = "" + json_obj.order_items[i].amount;
-                spaceCount = 6 - Wlen(count);
+				spaceCount = 0;
+                var amount = "" + json_obj.order_items[i].amount;//單一產品價格值轉字串
+                spaceCount = 6 - Wlen(count);//計算價格欄位的空白數= 該欄位總長度6 - 數量字串長度
                 for (var j = 0; j < spaceCount; j++) {
                     space += " ";
                 }
                 amount = space + json_obj.order_items[i].amount;
 
-                var product_name = json_obj.order_items[i].product_name;
-                //48字
+				//產品&包材;文字靠左 + 放大 + 產品 + 換行
                 space = "";
-                spaceCount = 48 - Wlen(product_name) - Wlen(count) - 4 - Wlen(amount);
+				spaceCount = 0;
+				var product_name = json_obj.order_items[i].product_name;
+                spaceCount = 48 - Wlen(product_name) - Wlen(count) - 4 - Wlen(amount);//該列總長度-產品民長度-數量長度-4-價格長度
                 for (var j = 0; j < spaceCount; j++) {
                     space += " ";
                 }
-                strbuf = product_name + space + count + "    " + amount;//48=32+6+4+6
-                ESC_Value.push(ecTEXT_ALIGN_LEFT + ecBIG_ON + strbuf + ecBIG_OFF + ecFREE_LINE);//文字靠左 + 放大 + 產品 + 換行
+                strbuf = product_name + space + count + "    " + amount;
+                ESC_Value.push(ecTEXT_ALIGN_LEFT + ecBIG_ON + strbuf + ecBIG_OFF + ecFREE_LINE);
 
-                strbuf = "  (";
+                //配料;文字靠左 + 配料 + 換行
+				strbuf = "  (";
                 if (json_obj.order_items[i].condiments != null) {
                     for (var k = 0; k < json_obj.order_items[i].condiments.length; k++) {
                         if (k > 0) {
@@ -111,15 +128,17 @@ function Main() {
                         }
                     }
                     strbuf = strbuf + ")"
-                    ESC_Value.push(ecTEXT_ALIGN_LEFT + strbuf + ecFREE_LINE);//文字靠左 + 配料 + 換行
+                    ESC_Value.push(ecTEXT_ALIGN_LEFT + strbuf + ecFREE_LINE);
                 }
             }
-            else if (json_obj.order_items[i].product_type == 'T') {//套餐類型
+            else if (json_obj.order_items[i].product_type == 'T') 
+			{//套餐類型
                 if ((json_obj.order_items[i].set_meals != null) && (json_obj.order_items[i].set_meals.length > 0)) {
                     for (var j = 0; j < json_obj.order_items[i].set_meals.length; j++) {
+						AllCount += json_obj.order_items[i].set_meals[j].count;
+						
                         space = "";
                         spaceCount = 0;
-                        AllCount += json_obj.order_items[i].set_meals[j].count;
                         var count = "" + json_obj.order_items[i].set_meals[j].count;
                         spaceCount = 6 - Wlen(count) - 2;
                         for (var k = 0; k < spaceCount; k++) {
@@ -190,13 +209,54 @@ function Main() {
     strbuf = '------------------------------------------------';
     ESC_Value.push(ecTEXT_ALIGN_LEFT + strbuf + ecFREE_LINE);//文字靠左 + 分隔線 + 換行
 
-    strbuf = "總計數量: " + AllCount;
-    ESC_Value.push(ecTEXT_ALIGN_LEFT + ecBIG_ON + strbuf + ecBIG_OFF + ecFREE_LINE);//文字靠左 + 放大 + 總計數量 + 換行
+	//商品總數量;文字靠左 + 總計數量 + 換行
+	space = "";
+    spaceCount = 48 - Wlen("商品總數量: ") - Wlen(""+AllCount);
+	for (var l = 0; l < spaceCount; l++){
+		space += " ";//產生對應空白字串
+	}	
+    strbuf = "商品總數量: " + space + AllCount;
+    ESC_Value.push(ecTEXT_ALIGN_LEFT + strbuf + ecFREE_LINE);
+	
+	//小計金額;文字靠左 + subtotal + 換行
+	space = "";
+    spaceCount = 48 - Wlen("小計金額: ") - Wlen(""+json_obj.subtotal);
+	for (var l = 0; l < spaceCount; l++){
+		space += " ";//產生對應空白字串
+	}		
+	strbuf = "小計金額: " + space + json_obj.subtotal;
+	ESC_Value.push(ecTEXT_ALIGN_LEFT + strbuf + ecFREE_LINE);
 
     strbuf = '------------------------------------------------';
     ESC_Value.push(ecTEXT_ALIGN_LEFT + strbuf + ecFREE_LINE);//文字靠左 + 分隔線 + 換行
-
-
+	
+	//服務費
+	if(json_obj.service_fee>0)
+	{
+		space = "";
+		spaceCount = 48 - Wlen("服務費(" + json_obj.service_rate + "%): ") - Wlen(""+json_obj.service_fee);
+		for (var l = 0; l < spaceCount; l++){
+			space += " ";//產生對應空白字串
+		}	
+		strbuf = "服務費(" + json_obj.service_rate + "%): " + space + json_obj.service_fee;
+		ESC_Value.push(ecTEXT_ALIGN_LEFT + strbuf + ecFREE_LINE);//文字靠左 + 分隔線 + 換行
+	}
+	
+    strbuf = '------------------------------------------------';
+    ESC_Value.push(ecTEXT_ALIGN_LEFT + strbuf + ecFREE_LINE);//文字靠左 + 分隔線 + 換行
+	
+	//總計
+	space = "";
+	spaceCount = 48 - Wlen("總計: ") - Wlen(""+json_obj.amount);
+	for (var l = 0; l < spaceCount; l++){
+		space += " ";//產生對應空白字串
+	}	
+	strbuf = "總計: " + space + json_obj.amount
+	ESC_Value.push(ecTEXT_ALIGN_LEFT + ecBIG_ON + strbuf + ecBIG_OFF + ecFREE_LINE)
+	
+    strbuf = '------------------------------------------------';
+    ESC_Value.push(ecTEXT_ALIGN_LEFT + strbuf + ecFREE_LINE);//文字靠左 + 分隔線 + 換行
+	
     var now = new Date();
     month = pad2(now.getMonth() + 1);//months (0-11)
     day = pad2(now.getDate());//day (1-31)
