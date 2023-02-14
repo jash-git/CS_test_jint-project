b64472db2f95a184d4de77250d4e81ec70e92c98
jash.liao <jash.liao@gmail.com>
2023-02-14 16:38:57 +0800

    Add/Modified 202302141638

--- Code/CS_test_Jint/Program.cs
+++ Code/CS_test_Jint/Program.cs
@@ -402,7 +402,7 @@ namespace CS_test_Jint
             engine.Execute(System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "Script" +
                 Path.DirectorySeparatorChar + "CommonFun.js"));
             engine.Execute(System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "Script" +
-                Path.DirectorySeparatorChar + "REPORT_57mm.js"));
+                Path.DirectorySeparatorChar + "WORK_57mm.js"));
 
             engine.SetValue("input", StrInput);
 
