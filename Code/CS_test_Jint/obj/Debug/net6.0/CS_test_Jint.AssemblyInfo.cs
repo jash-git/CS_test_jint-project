29546847f9097fb262805290ac897a8c844e7793
Jash.Liao <jash.liao@gmail.com>
2025-06-17 13:34:47 +0800

    Modified 202506171334

--- Code/CS_test_Jint/obj/Debug/net6.0/CS_test_Jint.AssemblyInfo.cs
+++ Code/CS_test_Jint/obj/Debug/net6.0/CS_test_Jint.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CS_test_Jint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2022.9.5.0633")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0182f7346bfad9dccd12a48f9fdb2778048a822")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21415f7676d809273d80028947800acb4cc8549e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CS_test_Jint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CS_test_Jint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2022.9.5.0633")]
